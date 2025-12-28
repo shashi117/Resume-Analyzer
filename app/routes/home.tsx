6d91a27741f7eb89e3e7646a907241abff7cc625
Shashi Ranjan Yadav <sry06052000@gmail.com>
2025-12-28 11:18:31 +0530

    Completed the landing page for resume-analyzer

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,34 @@
+import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import ResumeCards from "~/components/ResumeCards";
+import { resumes } from "@/constants";
+import type { Resume } from "@/types";
 
-export function meta({}: Route.MetaArgs) {
+
+export function meta({ }: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resume Analyzer" },
+    { name: "description", content: "Welcome to Resume Analyzer!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <Navbar />
+    <section className="main-section">
+      <div className="page-heading py-16">
+        <h1>Track Your Application & Resume Rating</h1>
+        <h2>Review your application and resume with AI</h2>
+      </div>
+
+      {resumes.length > 0 && (
+        <div className="resumes-section">
+          {resumes.map((resume: Resume) => (
+            <ResumeCards key={resume.id} resume={resume} />
+          ))}
+        </div>
+      )}
+    </section>
+  </main>;
+
 }
